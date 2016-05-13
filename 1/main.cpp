060c782906a1b9684733b27fb4cc009747f8a866
JakimychevNikolaj <jakimychev.nikolaj@gmail.com>
2016-05-13 12:45:59 +0300

    Update main.cpp

--- 1/main.cpp
+++ 1/main.cpp
@@ -5,18 +5,19 @@
 
 using namespace std;
 
-size_t N;
-size_t K;
+struct SizeOfMap {
+    pair <size_t, size_t> size;
+};
 
-vector<pair<int, int>> FindingFirstLineOfTrees(short **array, size_t &solution) {
+vector<pair<int, int>> FindingFirstLineOfTrees(short **array, size_t &solution, SizeOfMap & sizeOfMap) {
     vector<pair<int, int>> position;
     bool isFirstTreeFound = false;
     bool isFirstLineOfTreesAlreadyFound = false;
-    for (size_t linesCounter = 0; linesCounter < N; ++linesCounter) {
+    for (size_t linesCounter = 0; linesCounter < sizeOfMap.size.first; ++linesCounter) {
         if (isFirstLineOfTreesAlreadyFound) {
             break;
         }
-        for (size_t columnCounter = 0; columnCounter < K; ++columnCounter) {
+        for (size_t columnCounter = 0; columnCounter < sizeOfMap.size.second; ++columnCounter) {
             if (!isFirstTreeFound && array[linesCounter][columnCounter] == 1) {
                 isFirstTreeFound = true;
                 if (columnCounter != 0) {
@@ -33,7 +34,7 @@ vector<pair<int, int>> FindingFirstLineOfTrees(short **array, size_t &solution) 
             else if (isFirstTreeFound) {
                 if (array[linesCounter][columnCounter] == 1) {
                     ++solution;
-                    if (columnCounter == K) {
+                    if (columnCounter == sizeOfMap.size.second) {
                         position.push_back({linesCounter, columnCounter});
                         array[linesCounter][columnCounter] = 4;
                         ++solution;
@@ -63,23 +64,23 @@ vector<pair<int, int>> FindingFirstLineOfTrees(short **array, size_t &solution) 
     return position;
 }
 
-short **CreateArray(ifstream &inputFile) {
+short **CreateArray(ifstream &inputFile, SizeOfMap & sizeOfMap) {
     short **array;
     string str;
 
-    inputFile >> N;
-    inputFile >> K;
+    inputFile >> sizeOfMap.size.first;
+    inputFile >> sizeOfMap.size.second;
 
     getline(inputFile, str);
 
-    array = new short *[N];
-    for (int i = 0; i < N; ++i) {
-        array[i] = new short[K];
+    array = new short *[sizeOfMap.size.first];
+    for (int i = 0; i < sizeOfMap.size.first; ++i) {
+        array[i] = new short[sizeOfMap.size.first];
     }
 
-    for (int linesCounter = 0; (getline(inputFile, str) && linesCounter < N); ++linesCounter) {
+    for (int linesCounter = 0; (getline(inputFile, str) && linesCounter < sizeOfMap.size.first); ++linesCounter) {
         const char *ch = str.c_str();
-        for (size_t columnCounter = 0; columnCounter < K; ++columnCounter) {
+        for (size_t columnCounter = 0; columnCounter < sizeOfMap.size.first; ++columnCounter) {
             array[linesCounter][columnCounter] = short(ch[columnCounter] - '0');
         }
     }
@@ -91,14 +92,14 @@ bool itsTheSamePoint(const vector<pair<int, int>> &position, const pair<int, int
 }
 
 bool AreAnyOfNeighborsATree(const int &i, const int &j, const int &positionCounter, vector<pair<int, int>> &position,
-                            short **array) {
+                            short **array, SizeOfMap & sizeOfMap) {
     if (i > 0 && array[i - 1][j] == 1) {
         if (!itsTheSamePoint(position, {i, j})) {
             position.push_back(make_pair(i, j));
             return true;
         }
     }
-    else if (i < N && array[i + 1][j] == 1) {
+    else if (i < sizeOfMap.size.first && array[i + 1][j] == 1) {
         if (!itsTheSamePoint(position, {i, j})) {
             position.push_back(make_pair(i, j));
             return true;
@@ -110,13 +111,13 @@ bool AreAnyOfNeighborsATree(const int &i, const int &j, const int &positionCount
             return true;
         }
     }
-    else if (j < K && array[i][j + 1] == 1) {
+    else if (j < sizeOfMap.size.second && array[i][j + 1] == 1) {
         if (!itsTheSamePoint(position, {i, j})) {
             position.push_back(make_pair(i, j));
             return true;
         }
     }
-    else if (i < N && j < K && array[i + 1][j + 1] == 1) {
+    else if (i < sizeOfMap.size.first && j < sizeOfMap.size.second && array[i + 1][j + 1] == 1) {
         if (!itsTheSamePoint(position, {i, j})) {
             position.push_back(make_pair(i, j));
             return true;
@@ -129,14 +130,14 @@ bool AreAnyOfNeighborsATree(const int &i, const int &j, const int &positionCount
             return true;
         }
     }
-    else if (i < N && j > 0 &&
+    else if (i < sizeOfMap.size.first && j > 0 &&
              array[i + 1][j - 1] == 1) {
         if (!itsTheSamePoint(position, {i, j})) {
             position.push_back(make_pair(i, j));
             return true;
         }
     }
-    else if (i > 0 && j < K &&
+    else if (i > 0 && j < sizeOfMap.size.second &&
              array[i - 1][j + 1] == 1) {
         if (!itsTheSamePoint(position, {i, j})) {
             position.push_back(make_pair(i, j));
@@ -148,9 +149,9 @@ bool AreAnyOfNeighborsATree(const int &i, const int &j, const int &positionCount
 }
 
 
-void PrintResult(short **array, const size_t &solution) {
-    for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < K; ++j) {
+void PrintResult(short **array, const size_t &solution, SizeOfMap & sizeOfMap) {
+    for (int i = 0; i < sizeOfMap.size.first; ++i) {
+        for (int j = 0; j < sizeOfMap.size.first; ++j) {
             cout << array[i][j] << " ";
         }
         cout << endl;
@@ -158,27 +159,27 @@ void PrintResult(short **array, const size_t &solution) {
     cout << solution << endl;
 }
 
-void WavesAlgorithm(vector<pair<int, int>> &position, short **array, size_t &solution) {
+void WavesAlgorithm(vector<pair<int, int>> &position, short **array, size_t &solution, SizeOfMap & sizeOfMap) {
     for (int i = 0; i < position.size() && position.size() != 0; ++i) {
-        for (size_t linesCounter = 0; linesCounter < N && position.size() != 0; ++linesCounter) {
-            for (size_t columnCounter = 0; columnCounter < K && position.size() != 0; ++columnCounter) {
+        for (size_t linesCounter = 0; linesCounter < sizeOfMap.size.first && position.size() != 0; ++linesCounter) {
+            for (size_t columnCounter = 0; columnCounter < sizeOfMap.size.second && position.size() != 0; ++columnCounter) {
                 if (position[i].first > 0 && array[position[i].first - 1][position[i].second] == 0 &&
-                        AreAnyOfNeighborsATree(position[i].first - 1, position[i].second, i, position, array)) {
+                        AreAnyOfNeighborsATree(position[i].first - 1, position[i].second, i, position, array, sizeOfMap)) {
                     array[position.back().first][position.back().second] = 4;
                     ++solution;
                 }
-                else if (position[i].first < N && array[position[i].first + 1][position[i].second] == 0 &&
-                        AreAnyOfNeighborsATree(position[i].first + 1, position[i].second, i, position, array)) {
+                else if (position[i].first < sizeOfMap.size.first && array[position[i].first + 1][position[i].second] == 0 &&
+                        AreAnyOfNeighborsATree(position[i].first + 1, position[i].second, i, position, array, sizeOfMap)) {
                     array[position.back().first][position.back().second] = 4;
                     ++solution;
                 }
                 else if (position[i].second > 0 && array[position[i].first][position[i].second - 1] == 0 &&
-                        AreAnyOfNeighborsATree(position[i].first, position[i].second - 1, i, position, array)) {
+                        AreAnyOfNeighborsATree(position[i].first, position[i].second - 1, i, position, array, sizeOfMap)) {
                     array[position.back().first][position.back().second] = 4;
                     ++solution;
                 }
-                else if (position[i].second < K && array[position[i].first][position[i].second + 1] == 0 &&
-                        AreAnyOfNeighborsATree(position[i].first, position[i].second + 1, i, position, array)) {
+                else if (position[i].second < sizeOfMap.size.second && array[position[i].first][position[i].second + 1] == 0 &&
+                        AreAnyOfNeighborsATree(position[i].first, position[i].second + 1, i, position, array, sizeOfMap)) {
                     array[position.back().first][position.back().second] = 4;
                     ++solution;
                 }
@@ -199,12 +200,13 @@ int main(int argc, char *argv[]) {
     }
 
     size_t solution = 0;
+    SizeOfMap sizeOfMap;
 
-    short **array = CreateArray(inputFile);
-    vector<pair<int, int>> position = FindingFirstLineOfTrees(array, solution);
+    short **array = CreateArray(inputFile, sizeOfMap);
+    vector<pair<int, int>> position = FindingFirstLineOfTrees(array, solution, sizeOfMap);
 
-    WavesAlgorithm(position, array, solution);
-    PrintResult(array, solution);
+    WavesAlgorithm(position, array, solution, sizeOfMap);
+    PrintResult(array, solution, sizeOfMap);
 
     return 0;
 }
